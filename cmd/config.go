bb5955f9ef04ae2550d15eedd9d0de6292c6fea4
lhk18451200751 <2521592082@qq.com>
2025-07-18 09:25:42 +0800

    更新代码

---
+++ cmd/config.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+// Config 定义配置结构体
+type Config struct {
+	Endpoint         string
+	Codec            string
+	BreakOnVad       bool
+	Speaker          string
+	Record           bool
+	TTSProvider      string
+	ASRProvider      string
+	ASREndpoint      string
+	ASRAppID         string
+	ASRSecretID      string
+	ASRSecretKey     string
+	ASRModelType     string
+	TTSEndpoint      string
+	TTSAppID         string
+	TTSSecretID      string
+	TTSSecretKey     string
+	VADModel         string
+	VADEndpoint      string
+	VADSecretKey     string
+	EndpointHost     string
+	EndpointSecurity bool
+	Logger           *logrus.Logger
+	Ctx              context.Context
+	Cancel           context.CancelFunc
+}
+
+func LoadConfig() (*Config, error) {
+	// 加载 .env 环境变量文件
+	godotenv.Load()
+
+	// 命令行设置
+	// ws://175.27.250.177:8080
+	var endpoint string = "ws://192.168.1.134:8080"
+	var codec string = "g722"
+	var breakOnVad bool = false
+	var speaker string = "601003"
+	var record bool = false
+	var ttsProvider string = "tencent"
+	var asrProvider string = "tencent"
+	var asrEndpoint string = "asr.tencentcloudapi.com"
+	var asrAppID string = "1369156880"
+	var asrSecretID string = "YOUR_TENCENT_SECRET_ID"
+	var asrSecretKey string = "YOUR_TENCENT_SECRET_KEY"
+	var asrModelType string = "16k_zh"
+	var ttsEndpoint string = "tts.tencentcloudapi.com"
+	var ttsAppID string = "1369156880"
+	var ttsSecretID string = "YOUR_TENCENT_SECRET_ID"
+	var ttsSecretKey string = "YOUR_TENCENT_SECRET_KEY"
+	var vadModel string = "silero"
+	var vadEndpoint string = ""
+	var vadSecretKey string = ""
+
+	// 解析命令行参数，初始化各类变量
+	// 作用：加载.env文件中的环境变量，并通过命令行参数或默认值初始化所有配置项
+	flag.StringVar(&endpoint, "endpoint", endpoint, "Endpoint to connect to")
+	flag.StringVar(&codec, "codec", codec, "Codec to use: g722, pcmu")
+	flag.BoolVar(&breakOnVad, "break-on-vad", breakOnVad, "Break on VAD")
+	flag.BoolVar(&record, "record", record, "Record the call")
+	flag.StringVar(&ttsProvider, "tts", ttsProvider, "TTS provider to use: tencent, voiceapi")
+	flag.StringVar(&asrProvider, "asr", asrProvider, "ASR provider to use: tencent, voiceapi")
+	flag.StringVar(&speaker, "speaker", speaker, "Speaker to use")
+	flag.StringVar(&asrEndpoint, "asr-endpoint", asrEndpoint, "ASR endpoint to use")
+	flag.StringVar(&asrModelType, "asr-model-type", asrModelType, "ASR model type to use")
+	flag.StringVar(&asrAppID, "asr-app-id", asrAppID, "ASR app id to use")
+	flag.StringVar(&asrSecretID, "asr-secret-id", asrSecretID, "ASR secret id to use")
+	flag.StringVar(&asrSecretKey, "asr-secret-key", asrSecretKey, "ASR secret key to use")
+	flag.StringVar(&ttsEndpoint, "tts-endpoint", ttsEndpoint, "TTS endpoint to use")
+	flag.StringVar(&ttsAppID, "tts-app-id", ttsAppID, "TTS app id to use")
+	flag.StringVar(&ttsSecretID, "tts-secret-id", ttsSecretID, "TTS secret id to use")
+	flag.StringVar(&ttsSecretKey, "tts-secret-key", ttsSecretKey, "TTS secret key to use")
+	flag.StringVar(&vadModel, "vad-model", vadModel, "VAD model to use")
+	flag.StringVar(&vadEndpoint, "vad-endpoint", vadEndpoint, "VAD endpoint to use")
+	flag.StringVar(&vadSecretKey, "vad-secret-key", vadSecretKey, "VAD secret key to use")
+
+	flag.Parse()                  // 解析命令行参数
+	u, err := url.Parse(endpoint) // 解析URL字符串
+	if err != nil {
+		fmt.Printf("Failed to prase endpoint: %v", err)
+		os.Exit(1)
+	}
+	endpointHost := u.Host                                 // 提取主机名
+	endpointSecurity := strings.ToLower(u.Scheme) == "wss" // 判断是否使用安全协议wss
+
+	// 创建日志记录器和可取消的上下文对象，方便后续日志输出和优雅退出
+	logger := logrus.New()            // 创建一个新的 logrus.Logger 实例
+	logger.SetOutput(os.Stdout)       // 设置日志的输出目标为 标准输出（控制台）
+	logger.SetLevel(logrus.InfoLevel) // 设置日志的 最低记录级别（只有等于或高于该级别的日志会被记录）
+
+	// 创建上下文取消
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	config := &Config{
+		Endpoint:         endpoint,
+		Codec:            codec,
+		BreakOnVad:       breakOnVad,
+		Speaker:          speaker,
+		Record:           record,
+		TTSProvider:      ttsProvider,
+		ASRProvider:      asrProvider,
+		ASREndpoint:      asrEndpoint,
+		ASRAppID:         asrAppID,
+		ASRSecretID:      asrSecretID,
+		ASRSecretKey:     asrSecretKey,
+		ASRModelType:     asrModelType,
+		TTSEndpoint:      ttsEndpoint,
+		TTSAppID:         ttsAppID,
+		TTSSecretID:      ttsSecretID,
+		TTSSecretKey:     ttsSecretKey,
+		VADModel:         vadModel,
+		VADEndpoint:      vadEndpoint,
+		VADSecretKey:     vadSecretKey,
+		EndpointHost:     endpointHost,
+		EndpointSecurity: endpointSecurity,
+		Logger:           logger,
+		Ctx:              ctx,
+		Cancel:           cancel,
+	}
+
+	return config, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
+	// "flag"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
+	// "net/url"
 	"os"
-	"strings"
+	// "strings"
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	"github.com/pion/webrtc/v3"
 	"github.com/restsend/rustpbxgo"
 	"github.com/sirupsen/logrus"
@@ -130,69 +130,12 @@ func createClient(ctx context.Context, option CreateClientOption, id string, cal
 }
 
 func main() {
-	// 加载 .env 环境变量文件
-	godotenv.Load()
-	// 命令行设置
-	// ws://175.27.250.177:8080
-	var endpoint string = "ws://192.168.1.134:8080"
-	var codec string = "g722"
-	var breakOnVad bool = false
-	var speaker string = "601003"
-	var record bool = false
-	var ttsProvider string = "tencent"
-	var asrProvider string = "tencent"
-	var asrEndpoint string = "asr.tencentcloudapi.com"
-	var asrAppID string = "1369156880"
-	var asrSecretID string = "YOUR_TENCENT_SECRET_ID"
-	var asrSecretKey string = "YOUR_TENCENT_SECRET_KEY"
-	var asrModelType string = "16k_zh"
-	var ttsEndpoint string = "tts.tencentcloudapi.com"
-	var ttsAppID string = "1369156880"
-	var ttsSecretID string = "YOUR_TENCENT_SECRET_ID"
-	var ttsSecretKey string = "YOUR_TENCENT_SECRET_KEY"
-	var vadModel string = "silero"
-	var vadEndpoint string = ""
-	var vadSecretKey string = ""
-
-	// 解析命令行参数，初始化各类变量
-	// 作用：加载.env文件中的环境变量，并通过命令行参数或默认值初始化所有配置项
-	flag.StringVar(&endpoint, "endpoint", endpoint, "Endpoint to connect to")
-	flag.StringVar(&codec, "codec", codec, "Codec to use: g722, pcmu")
-	flag.BoolVar(&breakOnVad, "break-on-vad", breakOnVad, "Break on VAD")
-	flag.BoolVar(&record, "record", record, "Record the call")
-	flag.StringVar(&ttsProvider, "tts", ttsProvider, "TTS provider to use: tencent, voiceapi")
-	flag.StringVar(&asrProvider, "asr", asrProvider, "ASR provider to use: tencent, voiceapi")
-	flag.StringVar(&speaker, "speaker", speaker, "Speaker to use")
-	flag.StringVar(&asrEndpoint, "asr-endpoint", asrEndpoint, "ASR endpoint to use")
-	flag.StringVar(&asrModelType, "asr-model-type", asrModelType, "ASR model type to use")
-	flag.StringVar(&asrAppID, "asr-app-id", asrAppID, "ASR app id to use")
-	flag.StringVar(&asrSecretID, "asr-secret-id", asrSecretID, "ASR secret id to use")
-	flag.StringVar(&asrSecretKey, "asr-secret-key", asrSecretKey, "ASR secret key to use")
-	flag.StringVar(&ttsEndpoint, "tts-endpoint", ttsEndpoint, "TTS endpoint to use")
-	flag.StringVar(&ttsAppID, "tts-app-id", ttsAppID, "TTS app id to use")
-	flag.StringVar(&ttsSecretID, "tts-secret-id", ttsSecretID, "TTS secret id to use")
-	flag.StringVar(&ttsSecretKey, "tts-secret-key", ttsSecretKey, "TTS secret key to use")
-	flag.StringVar(&vadModel, "vad-model", vadModel, "VAD model to use")
-	flag.StringVar(&vadEndpoint, "vad-endpoint", vadEndpoint, "VAD endpoint to use")
-	flag.StringVar(&vadSecretKey, "vad-secret-key", vadSecretKey, "VAD secret key to use")
-
-	flag.Parse()                  // 解析命令行参数
-	u, err := url.Parse(endpoint) // 解析URL字符串
+	// 初始化设置
+	config, err := LoadConfig()
 	if err != nil {
-		fmt.Printf("Failed to prase endpoint: %v", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	endpointHost := u.Host                                 // 提取主机名
-	endpointSecurity := strings.ToLower(u.Scheme) == "wss" // 判断是否使用安全协议wss
-
-	// 创建日志记录器和可取消的上下文对象，方便后续日志输出和优雅退出
-	logger := logrus.New()            // 创建一个新的 logrus.Logger 实例
-	logger.SetOutput(os.Stdout)       // 设置日志的输出目标为 标准输出（控制台）
-	logger.SetLevel(logrus.InfoLevel) // 设置日志的 最低记录级别（只有等于或高于该级别的日志会被记录）
-
-	// 创建上下文取消
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	// 客户端选项和通话参数构建
 	// 构建 RustpbxGo 客户端的所有配置选项
@@ -200,13 +143,13 @@ func main() {
 	sigChan := make(chan bool) // 创建一个用于传递布尔类型数据的通道
 	// 给结构体实例赋值
 	option := CreateClientOption{
-		Endpoint:   endpoint,
-		Logger:     logger,
+		Endpoint:   config.Endpoint,
+		Logger:     config.Logger,
 		SigChan:    sigChan,
-		BreakOnVad: breakOnVad,
+		BreakOnVad: config.BreakOnVad,
 	}
 	var recorder *rustpbxgo.RecorderOption
-	if record {
+	if config.Record {
 		recorder = &rustpbxgo.RecorderOption{
 			Samplerate: 16000,
 		}
@@ -215,34 +158,34 @@ func main() {
 		Recorder: recorder,
 		Denoise:  true,
 		VAD: &rustpbxgo.VADOption{
-			Type:      vadModel,
-			Endpoint:  vadEndpoint,
-			SecretKey: vadSecretKey,
+			Type:      config.VADModel,
+			Endpoint:  config.VADEndpoint,
+			SecretKey: config.VADSecretKey,
 		},
 		ASR: &rustpbxgo.ASROption{
-			Provider:  asrProvider,
-			Endpoint:  asrEndpoint,
-			AppID:     asrAppID,
-			SecretID:  asrSecretID,
-			SecretKey: asrSecretKey,
-			ModelType: asrModelType,
+			Provider:  config.ASRProvider,
+			Endpoint:  config.ASREndpoint,
+			AppID:     config.ASRAppID,
+			SecretID:  config.ASRSecretID,
+			SecretKey: config.ASRSecretKey,
+			ModelType: config.ASRModelType,
 		},
 		TTS: &rustpbxgo.TTSOption{
-			Provider:  ttsProvider,
-			Speaker:   speaker,
-			Endpoint:  ttsEndpoint,
-			AppID:     ttsAppID,
-			SecretID:  ttsSecretID,
-			SecretKey: ttsSecretKey,
+			Provider:  config.TTSProvider,
+			Speaker:   config.Speaker,
+			Endpoint:  config.TTSEndpoint,
+			AppID:     config.TTSAppID,
+			SecretID:  config.TTSSecretID,
+			SecretKey: config.TTSSecretKey,
 		},
 	}
 	option.CallOption = callOption
 
 	// 媒体处理器初始化
 	// 创建媒体处理器，用于管理音频流和 SDP 协议
-	mediaHandler, err := NewMediaHandler(ctx, logger)
+	mediaHandler, err := NewMediaHandler(config.Ctx, config.Logger)
 	if err != nil {
-		logger.Fatalf("Failed to create media handler: %v", err)
+		config.Logger.Fatalf("Failed to create media handler: %v", err)
 	}
 	defer mediaHandler.Stop()
 
@@ -250,50 +193,115 @@ func main() {
 	var iceSevers []webrtc.ICEServer
 	// 获取 ICE 服务器列表并协商 SDP，准备 WebRTC 通话
 	iceUrl := "https://"
-	if !endpointSecurity {
+	if !config.EndpointSecurity {
 		iceUrl = "http://"
 	}
-	iceUrl = fmt.Sprintf("%s%s/icesevers", iceUrl, endpointHost)
+	iceUrl = fmt.Sprintf("%s%s/icesevers", iceUrl, config.EndpointHost)
 	resp, err := http.Get(iceUrl)
 	if err == nil {
 		defer resp.Body.Close()
 		body, _ := io.ReadAll(resp.Body)
 		json.Unmarshal(body, &iceSevers)
-		logger.WithFields(logrus.Fields{
+		config.Logger.WithFields(logrus.Fields{
 			"iceservers": len(iceSevers),
 		}).Info("get iceservers")
 	}
 
 	// 解析 SDP
-	localSdp, err := mediaHandler.Setup(codec, iceSevers)
+	localSdp, err := mediaHandler.Setup(config.Codec, iceSevers)
 	if err != nil {
-		logger.Fatalf("Failed to get local SDP: %v", err)
+		config.Logger.Fatalf("Failed to get local SDP: %v", err)
 	}
-	logger.Infof("Offer SDP: %v", localSdp)
+	config.Logger.Infof("Offer SDP: %v", localSdp)
 	callOption.Offer = localSdp
 
 	// 创建 RustpbxGo 客户端连接服务器，通话结束后自动关闭
-	client := createClient(ctx, option, "", callOption)
+	client := createClient(config.Ctx, option, "", callOption)
 	// 连接服务器
 	err = client.Connect(callType)
 	if err != nil {
-		logger.Fatalf("Failed to connect to server: %v", err)
+		config.Logger.Fatalf("Failed to connect to server: %v", err)
 	}
 	defer client.Shutdown()
 
 	// 发起通话请求，收到服务器应答后进行 SDP 协商（WebRTC）
-	answer, err := client.Invite(ctx, callOption)
+	answer, err := client.Invite(config.Ctx, callOption)
 	if err != nil {
-		logger.Fatalf("Failed to invite: %v", err)
+		config.Logger.Fatalf("Failed to invite: %v", err)
 	}
-	logger.Infof("Answer SDP: %v", answer.Sdp)
+	config.Logger.Infof("Answer SDP: %v", answer.Sdp)
 
 	// ICE服务器获取与SDP协商
 	err = mediaHandler.SetupAnswer(answer.Sdp)
 	if err != nil {
-		logger.Fatalf("Failed to setup answer: %v", err)
+		config.Logger.Fatalf("Failed to setup answer: %v", err)
 	}
 
 	<-sigChan
 	fmt.Println("Shutting down...")
 }
+
+
+// 加载 .env 环境变量文件
+	// godotenv.Load()
+	// // 命令行设置
+	// // ws://175.27.250.177:8080
+	// var endpoint string = "ws://192.168.1.134:8080"
+	// var codec string = "g722"
+	// var breakOnVad bool = false
+	// var speaker string = "601003"
+	// var record bool = false
+	// var ttsProvider string = "tencent"
+	// var asrProvider string = "tencent"
+	// var asrEndpoint string = "asr.tencentcloudapi.com"
+	// var asrAppID string = "1369156880"
+	// var asrSecretID string = "YOUR_TENCENT_SECRET_ID"
+	// var asrSecretKey string = "YOUR_TENCENT_SECRET_KEY"
+	// var asrModelType string = "16k_zh"
+	// var ttsEndpoint string = "tts.tencentcloudapi.com"
+	// var ttsAppID string = "1369156880"
+	// var ttsSecretID string = "YOUR_TENCENT_SECRET_ID"
+	// var ttsSecretKey string = "YOUR_TENCENT_SECRET_KEY"
+	// var vadModel string = "silero"
+	// var vadEndpoint string = ""
+	// var vadSecretKey string = ""
+
+	// // 解析命令行参数，初始化各类变量
+	// // 作用：加载.env文件中的环境变量，并通过命令行参数或默认值初始化所有配置项
+	// flag.StringVar(&endpoint, "endpoint", endpoint, "Endpoint to connect to")
+	// flag.StringVar(&codec, "codec", codec, "Codec to use: g722, pcmu")
+	// flag.BoolVar(&breakOnVad, "break-on-vad", breakOnVad, "Break on VAD")
+	// flag.BoolVar(&record, "record", record, "Record the call")
+	// flag.StringVar(&ttsProvider, "tts", ttsProvider, "TTS provider to use: tencent, voiceapi")
+	// flag.StringVar(&asrProvider, "asr", asrProvider, "ASR provider to use: tencent, voiceapi")
+	// flag.StringVar(&speaker, "speaker", speaker, "Speaker to use")
+	// flag.StringVar(&asrEndpoint, "asr-endpoint", asrEndpoint, "ASR endpoint to use")
+	// flag.StringVar(&asrModelType, "asr-model-type", asrModelType, "ASR model type to use")
+	// flag.StringVar(&asrAppID, "asr-app-id", asrAppID, "ASR app id to use")
+	// flag.StringVar(&asrSecretID, "asr-secret-id", asrSecretID, "ASR secret id to use")
+	// flag.StringVar(&asrSecretKey, "asr-secret-key", asrSecretKey, "ASR secret key to use")
+	// flag.StringVar(&ttsEndpoint, "tts-endpoint", ttsEndpoint, "TTS endpoint to use")
+	// flag.StringVar(&ttsAppID, "tts-app-id", ttsAppID, "TTS app id to use")
+	// flag.StringVar(&ttsSecretID, "tts-secret-id", ttsSecretID, "TTS secret id to use")
+	// flag.StringVar(&ttsSecretKey, "tts-secret-key", ttsSecretKey, "TTS secret key to use")
+	// flag.StringVar(&vadModel, "vad-model", vadModel, "VAD model to use")
+	// flag.StringVar(&vadEndpoint, "vad-endpoint", vadEndpoint, "VAD endpoint to use")
+	// flag.StringVar(&vadSecretKey, "vad-secret-key", vadSecretKey, "VAD secret key to use")
+
+	// flag.Parse()                  // 解析命令行参数
+	// u, err := url.Parse(endpoint) // 解析URL字符串
+	// if err != nil {
+	// 	fmt.Printf("Failed to prase endpoint: %v", err)
+	// 	os.Exit(1)
+	// }
+	// endpointHost := u.Host                                 // 提取主机名
+	// endpointSecurity := strings.ToLower(u.Scheme) == "wss" // 判断是否使用安全协议wss
+
+	// // 创建日志记录器和可取消的上下文对象，方便后续日志输出和优雅退出
+	// logger := logrus.New()            // 创建一个新的 logrus.Logger 实例
+	// logger.SetOutput(os.Stdout)       // 设置日志的输出目标为 标准输出（控制台）
+	// logger.SetLevel(logrus.InfoLevel) // 设置日志的 最低记录级别（只有等于或高于该级别的日志会被记录）
+
+	// // 创建上下文取消
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
